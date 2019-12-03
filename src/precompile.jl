34f8ebbba0151f800a3ca9b4146f671b781453aa
Chris Elrod <elrodc@gmail.com>
2019-12-03 16:51:55 -0500

    Removed some precompile statements.

--- src/precompile.jl
+++ src/precompile.jl
@@ -1,7 +1,7 @@
 function _precompile_()
     ccall(:jl_generating_output, Cint, ()) == 1 || return nothing
-    precompile(Tuple{QuasiNewtonMethods.var"##s39#7",Any,Any,Any,Any,Any,Any,Any,Any,Any,Any,Any,Any,Any})
+    # precompile(Tuple{QuasiNewtonMethods.var"##s39#7",Any,Any,Any,Any,Any,Any,Any,Any,Any,Any,Any,Any,Any})
     precompile(Tuple{typeof(QuasiNewtonMethods.BFGS_update_quote),Int64,Int64,Type})
-    precompile(Tuple{QuasiNewtonMethods.var"##s16#1",Any,Any,Any,Any,Any,Any,Any,Any,Any,Any})
+    # precompile(Tuple{QuasiNewtonMethods.var"##s16#1",Any,Any,Any,Any,Any,Any,Any,Any,Any,Any})
     precompile(Tuple{typeof(QuasiNewtonMethods.bfgs_column_update_block),Int64,Type,Int64,Int64,Symbol,Symbol,Symbol})
 end
